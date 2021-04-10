73fbdbc85faedfb9b611e8e7a2403cbf68dadb32
Megan Zinka <meganzinka@gmail.com>
2021-04-09 23:31:17 -0400

    portfolio created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import SideBar from './components/SideBar';
+import Home from './components/Home';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Logo from "./components/Logo"
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div id="app-container">
+				<Logo />
+				<div id = "sidebar-container">
+					<SideBar />
+				</div>
+				<div id="main-container">
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/resume" component={Resume} />
+						<Route path="/projects" component={Projects} />
+					</Switch>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import threeCircles from "../images/yellowcircles.png";
+import logo from "../images/logo.png";
+
+const Home = () => {
+	return (
+		<div>
+			<div class="title">
+				<img class="logo" src={logo} />
+				<br></br>
+				<h3>ABOUT ME</h3>
+				<p>
+					After six years in the urban education field as an Advanced Algebra and AP Statistics teacher, I am
+					excited to transition into web development. After self-directed learning for months, I recently
+					completed an intensive web development bootcamp program where I could fully immerse myself in the
+					field. I found my background in mathematics and statistics not only to be incredibly useful in
+					understanding the type of problem solving that web development demands, and I revelled in the
+					opportunity to think critically, logically, and mathematically on a daily basis. The more I learn
+					about web development, I find new interests and passions, such as sleek design, building intuitive
+					and efficient systems. I am excited to continue to learn and grow in the field!
+				</p>
+
+				<img class ="three-circles" src={threeCircles} />
+				<h3>CONTACT INFO</h3>
+
+			</div>
+		</div>
+	);
+};
+
+export default Home;
